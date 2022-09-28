97c616cbfedab4380f5100b7b35c2c9f364d3ec9
Mario-dev-app <76742164+Mario-dev-app@users.noreply.github.com>
2022-09-27 19:58:28 -0500

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-  database: "dncka30tc1qgs",
-  username: "ppwdvkenrrviph",
-  password: "522318a9098556dd03c849dcfd676426037224345308b13819ad39cc4d2c3e1f",
-  host: "ec2-52-207-90-231.compute-1.amazonaws.com",
+  database: "database",
+  username: "username",
+  password: "password",
+  host: "host",
   port: 5432,
   dialect: "postgres",
   dialectOptions: {
