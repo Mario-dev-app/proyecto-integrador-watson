4e2f9f29307f353f674f32dc8c341af043d07e25
Mario Peralta <marioperaltadev@gmail.com>
2022-09-27 19:16:37 -0500

    make it better

--- routes/paciente.js
+++ routes/paciente.js
@@ -58,13 +58,53 @@ router.get('/paciente', (req, res) => {
 
 /* Registrar un paciente */
 router.post('/paciente', (req, res) => {
-    let paciente = req.body;
-    Paciente.sync();
+    /*
+    #swagger.parameters['nombre'] = {
+        in: 'body',
+        description: 'Nombre del paciente',
+        schema: { nombre: 'Mario Peralta Westreicher'}
+    }
+    */
+    /*
+    #swagger.parameters['correo'] = {
+        in: 'body',
+        description: 'Correo del paciente',
+        schema: { correo: 'mario@gmail.com'}
+    }
+    */
+    /*
+    #swagger.parameters['telefono'] = {
+        in: 'body',
+        description: 'Telefono del paciente',
+        schema: { telefono: '972914057'}
+    }
+    */
+    /*
+    #swagger.parameters['dni'] = {
+        in: 'body',
+        description: 'DNI del paciente',
+        schema: { dni: '72552743'}
+    }
+    */
+    let nombre = req.body.nombre;
+    let correo = req.body.correo;
+    let telefono = req.body.telefono;
+    let dni = req.body.dni;
+    console.log({
+        nombre,
+        correo,
+        telefono,
+        dni
+    });
+    res.json({
+        ok: true
+    });
+    /* Paciente.sync();
     Paciente.create({
-        nombre: paciente.nombre,
-        correo: paciente.correo,
-        telefono: paciente.telefono,
-        dni: paciente.dni
+        nombre: nombre,
+        correo: correo,
+        telefono: telefono,
+        dni: dni
     }).then(() => {
         res.json({
             ok: true,
@@ -75,7 +115,88 @@ router.post('/paciente', (req, res) => {
             ok: false,
             message: err
         });
-    })
+    }) */
 });
+
+let pacienteTemporal = {
+    nombre: '',
+    correo: '',
+    telefono: '',
+    dni: ''
+};
+
+/* Obtener nombre de paciente*/
+router.post('/registro/paciente-nombre', (req, res) => {
+    /*
+    #swagger.parameters['nombre'] = {
+        in: 'body',
+        description: 'Nombre del paciente',
+        schema: { nombre: 'Mario Peralta Westreicher'}
+    }
+    */
+   let nombre = req.body.nombre;
+   if(nombre){
+    pacienteTemporal.nombre = nombre;
+   };
+   res.json({
+    message: 'Se agregó nombre al temporal'
+   });
+});
+
+/* Obtener correo de paciente */
+router.post('/registro/paciente-correo', (req, res) => {
+    /*
+    #swagger.parameters['correo'] = {
+        in: 'body',
+        description: 'Correo del paciente',
+        schema: { correo: 'mario@gmail.com'}
+    }
+    */
+    let correo = req.body.correo;
+    if(correo){
+     pacienteTemporal.correo = correo;
+    };
+    res.json({
+     message: 'Se agregó correo al temporal'
+    });
+});
+
+/* Obtener telefono de paciente */
+router.post('/registro/paciente-telefono', (req, res) => {
+    /*
+    #swagger.parameters['telefono'] = {
+        in: 'body',
+        description: 'Telefono del paciente',
+        schema: { telefono: '972914057'}
+    }
+    */
+    let telefono = req.body.telefono;
+    if(telefono){
+     pacienteTemporal.telefono = telefono;
+    };
+    res.json({
+     message: 'Se agregó telefono al temporal'
+    });
+});
+
+/* Obtener DNI de paciente */
+router.post('/registro/paciente-dni', (req, res) => {
+    /*
+    #swagger.parameters['dni'] = {
+        in: 'body',
+        description: 'DNI del paciente',
+        schema: { dni: '72552743'}
+    }
+    */
+    let dni = req.body.dni;
+    if(dni){
+     pacienteTemporal.nombre = dni;
+    };
+    res.json({
+     message: 'Se agregó nombre al temporal'
+    });
+});
+
+/* Guardar paciente nuevo */
 
 module.exports = router;
